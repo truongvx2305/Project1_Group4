8515070b064892963a0c2c8626483d2cc5ca8633
truongvx2305 <truongvxph51260@gmail.com>
2024-11-22 13:45:36 +0700

    update

--- app/src/main/java/com/example/project1/Model/CustomerModel.java
+++ app/src/main/java/com/example/project1/Model/CustomerModel.java
@@ -4,14 +4,17 @@ public class CustomerModel {
     private int id;
     private String name;
     private String phoneNumber;
+    private boolean isVIP;
 
     public CustomerModel() {
     }
 
-    public CustomerModel(int id, String name, String phoneNumber) {
+    public CustomerModel(int id, String name, String phoneNumber, boolean isVIP) {
         this.id = id;
         this.name = name;
         this.phoneNumber = phoneNumber;
+        this.isVIP = isVIP;
+
     }
 
     public int getId() {
@@ -36,5 +39,17 @@ public class CustomerModel {
 
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
+    }
+
+    public boolean isVIP() {
+        return isVIP;
+    }
+
+    public void setVIP(boolean VIP) {
+        isVIP = VIP;
+    }
+
+    public String getStatus() {
+        return this.isVIP ? "Khách hàng VIP" : "Khách hàng thường";
     }
 }
